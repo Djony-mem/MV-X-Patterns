8eae0323766ccd38fca20148c003be5429321ed0
Eugeniya <bruyko.e@gmai.com>
2021-10-14 22:08:53 +0300

    MVVM Example

---
+++ MV(X)Patterns/GreetingViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  GreetingViewModel.swift
+//  MV(X)Patterns
+//
+//  Created by brubru on 14.10.2021.
+//
+
+import Foundation
+
+protocol GreetingViewModelProtocol {
+    var greeting: String? { get }
+    
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)? { get set }
+    
+    init(person: Person)
+    func showGreeting()
+}
+
+class GreetingViewModel: GreetingViewModelProtocol {
+    var greeting: String? {
+        didSet {
+            greetingDidChange?(self)
+        }
+    }
+    
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)?
+    
+    private let person: Person
+    
+    required init(person: Person) {
+        self.person = person
+    }
+    
+    func showGreeting() {
+        greeting = "Hello, \(person.name) \(person.surname)!"
+    }
+
+}
